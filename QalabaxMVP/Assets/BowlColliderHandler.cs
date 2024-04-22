a5f3f7a8d70343c7921e10bfc84a652ade49d77a
a2mirza <mustafa.mirza2169@gmail.com>
2024-04-22 08:18:14 +0400

    Serving Update
    Savepoint before i f# things up

---
+++ QalabaxMVP/Assets/BowlColliderHandler.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class BowlColliderHandler : MonoBehaviour
+{
+    // Reference to the empty child object of the Bowl prefab
+    public Transform emptyChild;
+
+    // This method is called whenever a trigger collider enters another trigger collider
+    private void OnTriggerEnter(Collider other)
+    {
+        // Check if the collider that entered is a cooked noodles pot object
+        if (other.CompareTag("CookedNoodles"))
+        {
+            // Get the prefab of the pot object
+            GameObject potPrefab = other.gameObject;
+
+            // Clone the prefab and position it on the empty child object of the Bowl
+            GameObject clonedPrefab = Instantiate(potPrefab, emptyChild.position, emptyChild.rotation, emptyChild);
+
+            // Alter the dimension of the cloned prefab as needed
+            // For example, you can change the scale of the cloned prefab
+            clonedPrefab.transform.localScale = new Vector3(1.5f, 1.5f, 1.5f);
+        }
+    }
+}
